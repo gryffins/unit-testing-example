870b4a63c6e4cad671097bcc126af146ffb5a1f8
Alex <44375893+gryffins@users.noreply.github.com>
2018-12-10 17:07:30 +0000

    Implement multiply function and tests

--- index.test.js
+++ index.test.js
@@ -1,6 +1,7 @@
 const {
   sum,
-  subtract
+  subtract,
+  multiply
 } = require('./index');
 
 test('the sum of 1 and 1 should equal 2.', () => {
@@ -9,4 +10,8 @@ test('the sum of 1 and 1 should equal 2.', () => {
 
 test('subtract 1 from 5 to equal 4', () => {
   expect(subtract(5, 1)).toBe(4);
+});
+
+test('multiplies 5 and 2 to make 10', () => {
+  expect(multiply(5, 2)).toBe(10);
 });
