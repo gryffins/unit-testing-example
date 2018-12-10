ffaec3e51a7b9bcd60a0ed9df2f7e0542b23d8f6
Alex <44375893+gryffins@users.noreply.github.com>
2018-12-10 17:03:30 +0000

    Implement subtract function and tests

--- index.test.js
+++ index.test.js
@@ -1,5 +1,12 @@
-const sum = require('./index');
+const {
+  sum,
+  subtract
+} = require('./index');
 
 test('the sum of 1 and 1 should equal 2.', () => {
   expect(sum(1, 1)).toBe(2);
+});
+
+test('subtract 1 from 5 to equal 4', () => {
+  expect(subtract(5, 1)).toBe(4);
 });
